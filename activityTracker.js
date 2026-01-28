5664959710d65d670213a5736f887f29339a3932
veyiza <islurwhenifartt@gmail.com>
2026-01-27 23:05:22 -0800

    activityTracker.js

---
+++ activityTracker.js
@@ -0,0 +1,6 @@
+const myWeek = [
+  { day: "Monday", activity: "Jogging", category: "physical", hoursSpent: 1.5, enjoyment: 8, timeOfDay: "morning" },
+  { day: "Tuesday", activity: "Drawing", category: "creative", hoursSpent: 2, enjoyment: 9, timeOfDay: "evening" },
+  { day: "Wednesday", activity: "Hanging out with friends", category: "social", hoursSpent: 3, enjoyment: 10, timeOfDay: "afternoon" },
+  { day: "Thursday", activity: "Gaming", category: "social", hoursSpent: 2.5, enjoyment: 8, timeOfDay: "evening" },
+ 
