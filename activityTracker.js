d7ab196a1cf85a4479b4a9b59e3090d7fab278e4
veyiza <islurwhenifartt@gmail.com>
2026-01-27 23:15:53 -0800

    Added Predictions

--- activityTracker.js
+++ activityTracker.js
@@ -1,6 +1,14 @@
 const myWeek = [
-  { day: "Monday", activity: "Jogging", category: "physical", hoursSpent: 1.5, enjoyment: 8, timeOfDay: "morning" },
+  { day: "Monday", activity: "Walk", category: "physical", hoursSpent: 1.5, enjoyment: 8, timeOfDay: "morning" },
   { day: "Tuesday", activity: "Drawing", category: "creative", hoursSpent: 2, enjoyment: 9, timeOfDay: "evening" },
   { day: "Wednesday", activity: "Hanging out with friends", category: "social", hoursSpent: 3, enjoyment: 10, timeOfDay: "afternoon" },
   { day: "Thursday", activity: "Gaming", category: "social", hoursSpent: 2.5, enjoyment: 8, timeOfDay: "evening" },
- 
+  { day: "Friday", activity: "Studying", category: "productive", hoursSpent: 4, enjoyment: 4, timeOfDay: "afternoon" },
+  { day: "Saturday", activity: "Skating", category: "physical", hoursSpent: 3, enjoyment: 9, timeOfDay: "morning" },
+  { day: "Sunday", activity: "Watching Youtube", category: "relaxing", hoursSpent: 2.5, enjoyment: 7, timeOfDay: "afternoon" }
+];
+
+// Predictions
+// I think Hanging out with friends will have the highest enjoyment out of all activities.
+// The category that'll dominate most of my week will be physical.
+// A pattern I suspect is that afternoons have higher overall enjoyment because thats usually when I have the most free time.
